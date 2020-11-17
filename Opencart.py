00ad10d8b644156a8982ae4b6f714d3ec519b134
Sergey Sukhartev <qwerty255000@gmail.com>
2020-11-17 12:35:11 +0300

    added admin login logout

--- Opencart.py
+++ Opencart.py
@@ -10,7 +10,12 @@ class OpenCartLocators:
     LOCATOR_OPENCART_ADMIN_PASS = (By.ID, "input-password")
     LOCATOR_OPENCART_ADMIN_BUTTON = (By.TAG_NAME, "button")
     LOCATOR_OPENCART_ADMIN_LOGOUT_BUTTON = (By.CSS_SELECTOR, "#header > div > ul > li:nth-child(2) > a")
-
+    LOCATOR_OPENCART_ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, "#content > div.row > div:nth-child(1) > div > div.button-group > button:nth-child(1)")
+    LOCATOR_OPENCART_ADD_TO_CART_ALERT = (By.CLASS_NAME, "alert alert-success alert-dismissible")
+    LOCATOR_OPENCART_CART_BUTTON = (By.XPATH, '//*[@id="cart"]/button')
+    LOCATOR_OPENCART_CART_BUTTON_2 = (By.CSS_SELECTOR, "fa fa-shopping-cart")
+    LOCATOR_OPENCART_CART_TOTAL = (By.CSS_SELECTOR, "#content > div.row > div > table > tbody > tr:nth-child(4) > td:nth-child(2)")
+    LOCATOR_OPENCART_CART_BACK_TO_MAIN_BUTTON = (By.CLASS_NAME, "btn btn-default")
 
 class Search(BasePage):
 
@@ -47,3 +52,29 @@ class AdminLoginLogout(BasePage):
     def logout(self):
         logout = self.find_element(OpenCartLocators.LOCATOR_OPENCART_ADMIN_LOGOUT_BUTTON, time=2)
         logout.click()
+
+
+class AddToCart(BasePage):
+
+    def click_on_add_to_cart_button(self):
+        return self.find_element(OpenCartLocators.LOCATOR_OPENCART_ADD_TO_CART_BUTTON, time=2).click()
+
+    def click_on_cart_button(self):
+        self.find_element(OpenCartLocators.LOCATOR_OPENCART_CART_BUTTON, time=2).click()
+        self.find_element(OpenCartLocators.LOCATOR_OPENCART_CART_BUTTON_2, time=2).click()
+
+    def check_alert(self):
+        return self.find_element(OpenCartLocators.LOCATOR_OPENCART_ADD_TO_CART_ALERT, time=2)
+
+    @property
+    def find_total(self):
+        return self.find_element(OpenCartLocators.LOCATOR_OPENCART_CART_TOTAL, time=2)
+
+    def click_on_back_to_main_button(self):
+        self.find_element(OpenCartLocators.LOCATOR_OPENCART_CART_BACK_TO_MAIN_BUTTON, time=2).click()
+
+
+
+
+
+
--- tests/test_pom.py
+++ tests/test_pom.py
@@ -1,4 +1,4 @@
-from Opencart import Search, AdminLoginLogout
+from Opencart import Search, AdminLoginLogout, AddToCart
 import pytest
 
 
@@ -21,3 +21,14 @@ def test_admin_login(browser, base_url):
     opencart_admin_page.logout()
     assert opencart_admin_page.title == 'Administration'
 
+
+def test_add_to_cart(browser,base_url):
+    opencart_main_page = AddToCart(browser, base_url)
+    opencart_main_page.go_to_site()
+    opencart_main_page.click_on_add_to_cart_button()
+    #opencart_main_page.check_alert()
+    opencart_main_page.click_on_cart_button()
+    assert opencart_main_page.title == "Shopping Cart"
+    assert opencart_main_page.find_total == "£454.10"
+    opencart_main_page.click_on_back_to_main_button()
+    assert opencart_main_page.title == "Your Store"
